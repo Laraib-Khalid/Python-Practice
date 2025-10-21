3ae5c86e3f80f10e588bffaba555b50b4ea90f9e
Laraib-Khalid <laraibkhalid7861@gmail.com>
2025-10-21 12:21:38 +0500

    f-string code.

---
+++ Doc_string/doc_string.py
@@ -0,0 +1,18 @@
+def add_numbers(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+
+    Parameters:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The sum of a and b.
+    """
+    return a + b
+
+a = 5
+b = 5
+# Accessing the docstring
+print(add_numbers.__doc__)
+print(f"Add Number {a} and {b} = {add_numbers(a, b)}")
