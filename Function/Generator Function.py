784d04ed0adfa72ff59f39868e86850fc1eccdce
Laraib-Khalid <laraibkhalid7861@gmail.com>
2025-11-05 11:03:17 +0500

    generator function code update

--- Function/Generator Function.py
+++ Function/Generator Function.py
@@ -3,6 +3,19 @@ def my_generator():
         yield i
 
 gen = my_generator()
+print(gen)
 print(next(gen))
 print(next(gen))
 print(next(gen))
+
+print("-" * 50)
+
+def test():
+    for i in range(1, 5):
+        yield i
+
+gen = test()
+for value in gen:
+    print(value)
+
+
