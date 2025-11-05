d5d400947d6b338572110e911eecfec30b46f839
Laraib-Khalid <laraibkhalid7861@gmail.com>
2025-11-05 11:55:33 +0500

    Exercise 11 code

---
+++ Exercise 11/Notification using plyer.py
@@ -0,0 +1,30 @@
+from plyer import notification
+import time
+
+while True:
+    notification.notify(
+        title="💧 Water Reminder",
+        message="Drink a glass of water to stay fresh & healthy!🌿",
+        timeout=10
+    )
+
+    # Wait 1 hour (change to 7200 for 2 hours)
+    time.sleep(5)
+
+
+
+# import winsound
+# from plyer import notification
+# import time
+#
+# while True:
+#     # Play beep sound (frequency, duration in ms)
+#     winsound.Beep(2000, 1000)
+#
+#     notification.notify(
+#         title="💧 Water Time!",
+#         message="Drink water now!",
+#         timeout=10
+#     )
+#
+#     time.sleep(3500)
---
+++ Exercise 11/Notification.py
@@ -0,0 +1,16 @@
+from win10toast import ToastNotifier
+import time
+
+# Create notifier object
+notifier = ToastNotifier()
+
+while True:
+    # Show notification
+    notifier.show_toast(
+        "💧 Water Reminder",
+        "Time to drink water! Stay hydrated 🌿",
+        duration=10  # Notification stays for 10 seconds
+    )
+
+    # Wait for 1 hour (3600 sec)
+    time.sleep(3600)
