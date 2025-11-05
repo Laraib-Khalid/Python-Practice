56ec112daead0c12c9f282c0ae850488c045b0c6
Laraib-Khalid <laraibkhalid7861@gmail.com>
2025-11-05 12:43:36 +0500

    Regular Expression code update

--- Regular Expression/Regular Expression.py
+++ Regular Expression/Regular Expression.py
@@ -3,8 +3,32 @@
 # To test regex patterns interactively:
 # https://regex101.com
 
-# 1️⃣ Check if a string contains a word
+# https://regexr.com/
 import re
+
+pattern = r"[A-Z]+yclone"
+text = '''Cyclone Dumazile was a strong tropical cyclone in the South-West Indian Ocean that affected Madagascar and Réunion in early March 2018. Dumazile originated from a cyclone Dyclone low-pressure area that formed near Agaléga on 27 February. It became a tropical disturbance on 2 March, and was named the next day after attaining tropical storm status. Dumazile reached its peak intensity on 5 March, with 10-minute sustained winds of 165 km/h (105 mph), 1-minute sustained winds of 205 km/h (125 mph), and a central atmospheric pressure of 945 hPa (27.91 inHg). As it tracked southeastwards, Dumazile weakened steadily over the next couple of days due to wind shear, and became a post-tropical cyclone on 7 March
+
+'''
+
+match = re.search(pattern, text)
+print(match)
+
+print("-" * 50)
+
+matches = re.finditer(pattern, text)
+for match in matches:
+  print(match.span())
+  print(text[match.span()[0]: match.span()[1]])
+
+
+
+print("-" * 50)
+
+
+
+# 1️⃣ Check if a string contains a word
+# import re
 
 text = "I love Python programming"
 
